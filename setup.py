1fc5cdb4b8a26fbbf3a38b8926e5344a71262df8
Myles Eftos <myles@madpilot.com.au>
2018-08-08 22:09:35 +1000

    Adding route type constants. Bumping to version 0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyptv3",
-    version="0.0.2",
+    version="0.0.4",
     author="Myles Eftos",
     author_email="myles@madpilot.com.au",
     description="Access the Public Transport Victoria API with Python 3",
