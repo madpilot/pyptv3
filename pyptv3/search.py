b490ff28ec51c0ee5e3af52b2de276cbec671603
Myles Eftos <myles@madpilot.com.au>
2018-08-10 22:58:43 +1000

    Wrapping search results

--- pyptv3/search.py
+++ pyptv3/search.py
@@ -8,4 +8,4 @@ class Search:
     def term(self, term, **kwargs):
         quoted = urllib.parse.quote(term)
         query_list = pyptv3.QueryParams.process_kwargs(**kwargs)
-        return self._client.get("/search/" + quoted, query_list)
+        return pyptv3.SearchResponse(self._client.get("/search/" + quoted, query_list))
--- tests/unit/test_search.py
+++ tests/unit/test_search.py
@@ -1,20 +1,22 @@
 import pytest
 from mock import Mock
-from pyptv3 import Search
+from pyptv3 import Search, SearchResponse
 
 class TestRuns:
     @pytest.fixture(scope="module")
     def client(self):
-        return Mock()
+        client = Mock()
+        client.get.return_value = {"stops": [], "routes": [], "outlets": [], "status": {"version": "3.0", "health": 1}}
+        return client
 
     def test_term(self, client):
-        Search(client).term("Tottenham")
+        assert Search(client).term("Tottenham").__class__ == SearchResponse
         client.get.assert_called_with("/search/Tottenham", [])
 
     def test_term_with_spaces(self, client):
-        Search(client).term("South Morang")
+        assert Search(client).term("South Morang").__class__ == SearchResponse
         client.get.assert_called_with("/search/South%20Morang", [])
 
     def test_term_with_kwargs(self, client):
-        Search(client).term("Tottenham", route_types=[0, 1], latitude=-31.000, longitude=148.000, max_distance=500, include_addresses=True, include_outlets=True, match_stop_by_suburb=True, match_route_by_suburb=True)
+        assert Search(client).term("Tottenham", route_types=[0, 1], latitude=-31.000, longitude=148.000, max_distance=500, include_addresses=True, include_outlets=True, match_stop_by_suburb=True, match_route_by_suburb=True).__class__ == SearchResponse
         client.get.assert_called_with("/search/Tottenham",[("route_types", 0), ("route_types", 1), ("latitude", -31.000), ("longitude", 148.000), ("max_distance", 500), ("include_addresses", "true"), ("include_outlets", "true"), ("match_stop_by_suburb", "true"), ("match_route_by_suburb", "true")])
