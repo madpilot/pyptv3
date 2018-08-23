e1a1c7761bfbb0c75d6d754563c86f9b89e9898b
Myles Eftos <myles@madpilot.com.au>
2018-08-23 15:03:33 +1000

    Bumping to version 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyptv3",
-    version="0.0.4",
+    version="0.1.0",
     author="Myles Eftos",
     author_email="myles@madpilot.com.au",
     description="Access the Public Transport Victoria API with Python 3",
