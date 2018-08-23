b8538d505ebb827fddb0d3d861cc222823018809
Myles Eftos <myles@madpilot.com.au>
2018-08-23 15:32:53 +1000

    Documenting client and async_client

--- pyptv3/async_client.py
+++ pyptv3/async_client.py
@@ -1,9 +1,21 @@
-import asyncio
-import aiohttp
+"""
+    Provides an asyncio compatible Client.
+"""
 import json
+import aiohttp
 import pyptv3
 
-class AsyncClient():
+def _handle_error(response):
+    if response.status == 400:
+        raise pyptv3.InvalidRequestError("Invalid Request")
+    elif response.status == 403:
+        raise pyptv3.AccessDeniedError("Access Denied")
+    elif response.status == 404:
+        raise pyptv3.NotFoundError("Not Found")
+    else:
+        raise pyptv3.UnknownError("Code: " + str(response.status) + " - unknown_error")
+
+class AsyncClient(): # pylint: disable=too-few-public-methods
     """
     Create an async client class that will get passed in to other API object"
 
@@ -14,11 +26,20 @@ class AsyncClient():
     Returns:
         client: The initialized client object. Pass this in to the API classes
     """
-    def __init__(self, loop, developer_id = None, api_key = None):
+    def __init__(self, loop, developer_id=None, api_key=None):
         self._loop = loop
         self._url_builder = pyptv3.UrlBuilder(developer_id, api_key)
 
-    async def get(self, path, query_params = None):
+    async def get(self, path, query_params=None):
+        """
+            Perform an API call
+
+            Args:
+                path (string): the path to GET
+                query_params (list): List of query parameters to send
+            Returns:
+                An AsyncIO Coroutine
+        """
         if query_params is None:
             query_params = []
 
@@ -28,15 +49,4 @@ class AsyncClient():
             async with client.get(url) as response:
                 if response.status == 200:
                     return json.loads(await response.read())
-                else:
-                    self._handle_error(response)
-
-    def _handle_error(self, response):
-        if response.status == 400:
-            raise pyptv3.InvalidRequestError("Invalid Request")
-        elif response.status == 403:
-            raise pyptv3.AccessDeniedError("Access Denied")
-        elif response.status == 404:
-            raise pyptv3.NotFoundError("Not Found")
-        else:
-            raise pyptv3.UnknownError("Code: " + str(response.status) + " - unknown_error")
+                _handle_error(response)
--- pyptv3/client.py
+++ pyptv3/client.py
@@ -1,21 +1,45 @@
+"""
+    Provides the Client. A client instance gets passed to every pyptv3 class
+    constructor.
+"""
+
 import urllib.request
 import json
 import pyptv3
 
-class Client():
-    """
-    Create a client class that will get passed in to other API object"
+def _handle_error(error):
+    if error.code == 400:
+        raise pyptv3.InvalidRequestError(error.reason)
+    elif error.code == 403:
+        raise pyptv3.AccessDeniedError(error.reason)
+    elif error.code == 404:
+        raise pyptv3.NotFoundError(error.reason)
+    else:
+        raise pyptv3.UnknownError("Code: " + str(error.code) + " - " + error.reason)
 
-    Args:
-        developer_id (string): Your PTV API developer id
-        api_key (string): Your PTV API api key
-    Returns:
-        client: The initialized client object. Pass this in to the API classes
+class Client(): # pylint: disable=too-few-public-methods
     """
-    def __init__(self, developer_id = None, api_key = None):
+        Create a client class that will get passed in to other API object"
+
+        Args:
+            developer_id (string): Your PTV API developer id
+            api_key (string): Your PTV API api key
+        Returns:
+            client: The initialized client object. Pass this in to the API classes
+    """
+    def __init__(self, developer_id=None, api_key=None):
         self._url_builder = pyptv3.UrlBuilder(developer_id, api_key)
 
-    def get(self, path, query_params = None):
+    def get(self, path, query_params=None):
+        """
+            Perform an API call
+
+            Args:
+                path (string): the path to GET
+                query_params (list): List of query parameters to send
+            Returns:
+                None
+        """
         if query_params is None:
             query_params = []
 
@@ -23,16 +47,6 @@ class Client():
 
         try:
             response = urllib.request.urlopen(url)
-            return json.loads(response.read().decode('utf-8'))
-        except urllib.error.HTTPError as e:
-            self._handle_error(e)
-
-    def _handle_error(self, error):
-        if error.code == 400:
-            raise pyptv3.InvalidRequestError(error.reason)
-        elif error.code == 403:
-            raise pyptv3.AccessDeniedError(error.reason)
-        elif error.code == 404:
-            raise pyptv3.NotFoundError(error.reason)
-        else:
-            raise pyptv3.UnknownError("Code: " + str(error.code) + " - " + error.reason)
+            return json.loads(response.read().decode("utf-8"))
+        except urllib.error.HTTPError as error:
+            _handle_error(error)
